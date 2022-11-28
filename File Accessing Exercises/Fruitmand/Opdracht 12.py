3e8904ed5c974d8217b048665e31dd64fd79a8e9
Chronizzy <chronizzy@gmail.com>
2022-11-28 16:58:43 +0100

    NO CAP

--- File Accessing Exercises/Fruitmand/Opdracht 12.py
+++ File Accessing Exercises/Fruitmand/Opdracht 12.py
@@ -2,13 +2,13 @@ from fruitmand2 import fruitmand
 
 kleurenDictionary = {'pink' : 'roze', 'green' : 'groene', 'brown' : 'bruine', 'yellow' : 'gele', 'orange' : 'oranje', 'purple' : 'paarse', 'black' : 'zwarte'}
 
-info = []
+fruitnameLength = []
 for i in fruitmand:
-    info.append((len(i['name'])))
-    info.sort(reverse = True)
+    fruitnameLength.append((len(i['name'])))
+    fruitnameLength.sort(reverse = True)
 
 
 for i in fruitmand:
-    if info[0] == len(i['name']):
-        print('De', i['name'], '( ' + str(info[0]) + 'letters )',  'heeft een', kleurenDictionary[i['color']], 'kleur en een gewicht van', (i['weight']) / 1000, 'kg' )
+    if fruitnameLength[0] == len(i['name']):
+        print('De', i['name'], '( ' + str(fruitnameLength[0]) + 'letters )',  'heeft een', kleurenDictionary[i['color']], 'kleur en een gewicht van', (i['weight']) / 1000, 'kg' )
 
